ac8117a1e324c38d4053eb3c7b9f5f48bf4ef504
Alexander Young <alex.buzov@icloud.com>
2021-06-15 14:25:41 -0700

    dictionary builder

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-# DictionaryBuilder
+## DictionaryBuilder
 
-A description of this package.
+```swift
+let launchStatus = Dictionary {
+    ["Weather": true]
+    ["Range": true]
+    
+    if hasCrew {
+        ["Medic": true]
+    }
+    
+    if #available(macOS 11.0, iOS 14.0, *) {
+        ["Latest Software": true]
+    }
+    
+    switch reuseCount {
+    case 0:
+        ["Unboxed": true]
+    default:
+        ["Repainted": true]
+    }
+}
+```
--- Tests/DictionaryBuilderTests/DictionaryBuilderTests.swift
+++ Tests/DictionaryBuilderTests/DictionaryBuilderTests.swift
@@ -1,11 +1,36 @@
-    import XCTest
-    @testable import DictionaryBuilder
+import XCTest
+import DictionaryBuilder
 
-    final class DictionaryBuilderTests: XCTestCase {
-        func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-            XCTAssertEqual(DictionaryBuilder().text, "Hello, World!")
+final class DictionaryBuilderTests: XCTestCase {
+    func testBuildBlockComponents() {
+        let hasCrew = true
+        let reuseCount = 69
+        
+        let launchStatus = Dictionary {
+            ["Weather": true]
+            ["Range": true]
+            
+            if hasCrew {
+                ["Medic": true]
+            }
+            
+            if #available(macOS 11.0, iOS 14.0, *) {
+                ["Latest Software": true]
+            }
+            
+            switch reuseCount {
+            case 0:
+                ["Unboxed": true]
+            default:
+                ["Repainted": true]
+            }
         }
+        
+        XCTAssertTrue(launchStatus.count == 5)
+        XCTAssertTrue(launchStatus["Weather"] == true)
+        XCTAssertTrue(launchStatus["Range"] == true)
+        XCTAssertTrue(launchStatus["Medic"] == true)
+        XCTAssertTrue(launchStatus["Latest Software"] == true)
+        XCTAssertTrue(launchStatus["Repainted"] == true)
     }
+}
